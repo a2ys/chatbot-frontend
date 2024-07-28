eb58596aa69b46fdd6d41a4e053ec21073c26b2b
Aayush Shukla <oreus.aks@gmail.com>
2024-07-29 02:19:36 +0530

    chore: add shadcn ui

---
+++ src/react/InputField.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="Email" />
+      <Button type="submit">Subscribe</Button>
+    </div>
+  );
+}
